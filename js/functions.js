96595d1fc6c437d4481755b8e32455b75af2b10b
deonj197 <deonj197@gmail.com>
2017-06-28 09:49:10 -0400

    Added new buttons and cleaned up a bit

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,19 @@
 /**
  * Created by deonj on 6/27/2017.
  */
-
+var map;
 function makeMap(){
     var mapOptions ={
         center: new google.maps.LatLng(42.3319740,-83.1982680),
         zoom:10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "resize", function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);
+});
